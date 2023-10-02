1bd3d2cdf9a2792b99806299077a45ee679d229a
callelimascca <callelimasccamaria13@gmail.com>
2023-10-02 12:22:55 -0500

    ejercicios practicos de tkinter

---
+++ INTERFACES_HDS/ejemplos.py
@@ -0,0 +1,23 @@
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+import tkinter as tk
+import os
+
+#CREACIÓN DE LA VENTANA:
+canvas = tk.Tk()
+canvas.geometry("550x660")
+canvas.title("NOTA♥☺")
+canvas.config(bg = "pink")
+ventana= tk.Tk()
+
+def saludar():
+    etiqueta.config(text="¡Hola, Maria!")
+
+boton = tk.Button(ventana, text="Saludo", command=saludar)
+boton.pack()
+
+
+etiqueta = tk.Label(ventana, text="")
+etiqueta.pack()
+
+ventana.mainloop()
