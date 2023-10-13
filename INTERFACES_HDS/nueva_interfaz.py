883b23d8bab2caf6db27515737c4474d295f32bd
callelimascca <callelimasccamaria13@gmail.com>
2023-10-13 10:17:07 -0500

    tkinter

---
+++ INTERFACES_HDS/nueva_interfaz.py
@@ -0,0 +1,10 @@
+# 1. import tkinter -> libreria para la creacion de interfaces grafica:
+
+from tkinter import *
+#instancia la clase
+ventana=Tk()
+#creo mis dos widget de orden inferior con la clase frame()
+#instancio el primer widget
+widget_uno= Frame()
+#usr metodo para montar
+
