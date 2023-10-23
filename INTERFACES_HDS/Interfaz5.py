124b63a39a661798ffea1ad1dfd4ca35510d5677
callelimascca <callelimasccamaria13@gmail.com>
2023-10-23 11:59:51 -0500

    ejercicio de tkinter

--- INTERFACES_HDS/Interfaz5.py
+++ INTERFACES_HDS/Interfaz5.py
@@ -1,22 +1,30 @@
 from tkinter import * # * sirve para importar todos las herramientas de tk
 
-def captura_dato():
-    text=texto_nombre.get()
-    mensaje= Label(ventana,text= f"hola,{text}")
-    mensaje.pack()
+
+def evaluar_edad():
+    edad=int(texto_edad.get())
+   
+    if edad >= 18:
+        mensaje= Label(ventana, text= " eres mayor de edad ☺")
+        mensaje.pack()
+        
+    else:
+        mensaje1= Label(ventana, text= " eres menor de edad :(")
+        mensaje1.pack()
+    
 
 ventana=Tk()
 ventana.geometry("300x300")
 ventana.title("♠♠♠")
 
-etiqueta=Label(ventana, text="Introduce tu Nombre: ")#.grid(row=0,column=0) # sirve para colocar y ordenar filas y columnas 
+etiqueta=Label(ventana, text="Introduce tu Edad: ")#.grid(row=0,column=0) # sirve para colocar y ordenar filas y columnas 
 etiqueta.pack()
-texto_nombre=Entry(ventana)
-texto_nombre.config(bg = "blue", fg="yellow") #fg es para cambiar el color de la letra
-texto_nombre.pack()
+texto_edad=Entry(ventana)
+texto_edad.config(bg = "blue", fg="yellow") #fg es para cambiar el color de la letra
+texto_edad.pack()
 
-boton_capturar=Button(ventana,text="Enviar", command= captura_dato)
-boton_capturar.pack()
+boton_evaluar=Button(ventana, text= "Evaluar", command= evaluar_edad)
+boton_evaluar.pack()
 
 # sirve para colocar y ordenar filas y columnas 
 # pack: apila y enpaqueta las etiquetas (recibe otras propiedades que grid)
