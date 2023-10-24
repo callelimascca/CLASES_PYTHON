725096dbe54ef739fac6fc55f26a1a051d39e053
lourdes <132000301+callelimascca@users.noreply.github.com>
2023-10-24 18:46:01 -0500

    °°°

--- INTERFACES_HDS/interfaz_usuario.py
+++ INTERFACES_HDS/interfaz_usuario.py
@@ -57,3 +57,34 @@ boton_limpiar=Button(ventana,text='Limpiar',command=limpiar).grid(row=3,column=1
 #boton_limpiar.config(bg='pink')
 
 ventana.mainloop()
+##########################################################################################
+def comprobar_datos():
+    text1=text_usuario.get()
+    text2=int(text_contra.get())
+    if text1 == usuario and text2==contra:
+        mensaje=Label(ventana,text="Bienvenido :)")      
+        mensaje.pack()
+    else:
+        mensaje=Label(ventana,text="El usuario o contraseña son incorrectas.....revisa de nuevo")      
+        mensaje.pack()
+
+usuario="Maria"
+contra=7379961
+
+etiqueta1=Label(ventana,text="Usuario")
+etiqueta1.pack()
+
+text_usuario=Entry(ventana)
+text_usuario.config(bg="black",fg="white")
+text_usuario.pack()
+
+etiqueta2=Label(ventana,text="Contraseña")
+etiqueta2.pack()
+
+text_contra=Entry(ventana, show="☺")
+text_contra.config(bg="black",fg="white")
+text_contra.pack()
+boton_capturar=Button(ventana,text="Comprovar",command=comprobar_datos)
+boton_capturar.pack()
+
+ventana.mainloop()
