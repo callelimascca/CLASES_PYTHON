6c03940fa2591170a91d9b10e61ea9684a6fac26
lourdes <132000301+callelimascca@users.noreply.github.com>
2023-10-24 15:23:25 -0500

    ****

--- POO-LP/ejec.py
+++ POO-LP/ejec.py
@@ -3,15 +3,22 @@ from tarea import *
 class Tiendas_comerciales:
      def tienda_gerente(self, tarea_tiendas,nombre_gerente):
           rpt=list(filter(lambda e:e ["Gerente"]==nombre_gerente,tarea_tiendas))
-          return rpt
+          return f" Las tiendas pertenecientes a China son:  {rpt}"
      
      def mas_categoria(self, tarea_tiendas):
           rpt= list(filter(lambda el: len(el ["Categoria"]) > 2, tarea_tiendas))
-          return rpt
+          return f" La(s) o tienda(s) con mayor categorias son: {rpt}"
      
      def ruc_nombre(self,tarea_tiendas):
           rpt=list(map(lambda a:{"RUC":a ["RUC"],"Nombre":a["Nombre"]},tarea_tiendas))
           return  f" los RUC de las tiendas son : {rpt} "
+          
+     def eliminar_tienda(self,tarea_tiendas, ruc):
+          rps=list(filter(lambda e:e ["RUC"] != ruc, tarea_tiendas))
+          return f""" La tienda fue eliminado:
+==========================================================================
+          """
+          
      
      def mostrar_todo(self,tarea_tiendas):
           mensaje=f"""
@@ -26,3 +33,5 @@ print(rpts.mostrar_todo(tiendas))
 print(rpts.tienda_gerente(tiendas, "China"))
 print(rpts.mas_categoria(tiendas))
 print(rpts.ruc_nombre(tiendas))
+print(rpts.eliminar_tienda(tiendas, 2435452))
+
