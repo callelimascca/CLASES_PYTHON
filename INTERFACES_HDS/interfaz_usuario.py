4198d778bb487064216981d7a5e6735857300a09
callelimascca <callelimasccamari13@gmail.com>
2023-10-24 22:32:47 -0500

    **

--- INTERFACES_HDS/interfaz_usuario.py
+++ INTERFACES_HDS/interfaz_usuario.py
@@ -1,63 +1,5 @@
 from tkinter import*
 
-def eval():
-    us=user.get()
-    cr=int(contra.get())
-    if cr ==2023:
-        mensaje=Label(ventana,text=f'''
-        Hola usuario {us}, 
-        la contraseña es correcta.
-        .....................................
-        Bienvenido 😁''') 
-        mensaje.grid(row=3)
-    else:
-        mensaje=Label(ventana,text=f'''
-        Hola usuario {us}
-        la contraseña es incorrecta.
-        .....................................
-        lo sentimos🙂''') 
-        mensaje.grid(row=3)
-
-def limpiar():
-    user.delete(0,END)
-    contra.delete(0,END)
-    user.focus()
-
-
-           
-ventana =Tk()
-ventana.title('CORREO😄')
-ventana.geometry("300x200")
-ventana.config(bg='grey')
-
-widget_dos=Frame()
-widget_dos.grid(row='2',column='0')
-widget_dos.config(height='30',width='30')
-widget_dos.config(bg='grey') 
-
-et=Label(ventana, text="USUARIO ")
-et.grid(row=0,column=0)
-
-etiqueta =Label(ventana, text="PASSWORD")
-etiqueta.grid(row=1,column=0)
-
-user=Entry(ventana)
-user.config(bg='black',fg='white')
-user.grid(row=0,column=1)
-
-contra=Entry(ventana,show='*')
-contra.config(bg='black',fg='white')
-contra.grid(row=1,column=1)
-
-boton=Button(ventana,text='Evaluar',command=eval)
-boton.grid(row=3,column=0)
-#boton.config(bg='pink')
-
-boton_limpiar=Button(ventana,text='Limpiar',command=limpiar).grid(row=3,column=1) 
-#boton_limpiar.config(bg='pink')
-
-ventana.mainloop()
-##########################################################################################
 def comprobar_datos():
     text1=text_usuario.get()
     text2=int(text_contra.get())
@@ -67,6 +9,11 @@ def comprobar_datos():
     else:
         mensaje=Label(ventana,text="El usuario o contraseña son incorrectas.....revisa de nuevo")      
         mensaje.pack()
+
+ventana =Tk()
+ventana.title('CORREO😄')
+ventana.geometry("300x200")
+ventana.config(bg='grey')
 
 usuario="Maria"
 contra=7379961
--- POO-LP/ejec.py
+++ POO-LP/ejec.py
@@ -2,13 +2,13 @@ from tarea import *
 
 class Tiendas_comerciales:
 
-          def mostrar_todo(self,tarea_tiendas):
-          mensaje=f"""
+     def mostrar_todo(self,tarea_tiendas):
+               mensaje=f"""
     TIENES {len(tarea_tiendas)} TIENDAS COMERCIALES
     LAS TIENDAS SON: 
     {tarea_tiendas}
 """
-          return mensaje
+               return mensaje
      
      def tienda_gerente(self, tarea_tiendas,nombre_gerente):
           rpt=list(filter(lambda e:e ["Gerente"]==nombre_gerente,tarea_tiendas))
@@ -27,13 +27,41 @@ class Tiendas_comerciales:
           return f""" La tienda fue eliminado:
 ==========================================================================
           """
-          
+     def actualizacion(self,tarea_tiendas):
+          actualizar=list(filter(lambda a:{"RUC":a["RUC"],"Nombre":a["Nombre"]},tarea_tiendas))
+          return f"SE ACTUALIZO CON EXITO {actualizar} "
+
+     def registrar_tienda(self,ruc,nombre,categoria,horario_atencion,gerente):
+          nuevo=len(tiendas) +1
+          tienda_nueva={
+          "RUC":ruc,
+          "Nombre":nombre,
+          "Categoria":categoria,
+          "Horario_atencion":horario_atencion,
+          "Gerente":gerente
+          }
+          registrar=tiendas.append(tienda_nueva)
+          return f""" LA TIENDA SE REGISTRO CON EXITO:
+===================================================================================
+          {tienda_nueva}"""
      
+     def actualizar_horario(self,tarea_tiendas,ruc,horario_atencion):
+          actualizacion= list(filter(lambda el:{"RUC":el[ruc],"Horario_atencion":el[horario_atencion]}, tarea_tiendas))
+          return  f" El horario se actualizo exitosamente {actualizacion}"
+           
+      
      
 rpts=Tiendas_comerciales()
+# print(rpts.mostrar_todo(tiendas))
+# print(rpts.tienda_gerente(tiendas, "China"))
+# print(rpts.mas_categoria(tiendas))
+# print(rpts.ruc_nombre(tiendas))
+# print(rpts.eliminar_tienda(tiendas, 5456456))
+# print(rpts.actualizacion(tiendas))
+# print(rpts.registrar_tienda(1237456,"Tienda 11",["Bodega", "farmacia"],{"dia":"7am-12m", "tarde":"2pm-7pm"},"LUNA"))
+#print(rpts.mostrar_todo(tiendas))
+print(rpts.actualizar_horario(tiendas,2134354,{"dia":"6am-12m", "tarde":"3pm-8pm"}))
 print(rpts.mostrar_todo(tiendas))
-print(rpts.tienda_gerente(tiendas, "China"))
-print(rpts.mas_categoria(tiendas))
-print(rpts.ruc_nombre(tiendas))
-print(rpts.eliminar_tienda(tiendas, 2435452))
+
+
 
