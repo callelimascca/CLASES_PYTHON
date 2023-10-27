c136096620f298ade02e98ae5de3b5fa1ecad606
callelimascca <callelimasccamaria13@gmail.com>
2023-10-27 10:13:12 -0500

    ****

--- INTERFACES_HDS/ejercicio_6.py
+++ INTERFACES_HDS/ejercicio_6.py
@@ -1,9 +1,11 @@
 from tkinter import *
 
-def sumar():
-    pass
+seleccion=IntVar()
 
-def restar():
+def calcular():
+    n1=int(num1_text.get())
+    n2=int(num2_text.get())
+    resultado= n1 + n2
     pass
 
 
@@ -25,5 +27,12 @@ num_3.pack()
 num3_text =Entry(ventana)
 num3_text.pack()
 
+rb_s=Radiobutton(ventana,text="Suma",value="Sumar",variable=seleccion)
+rb_s.pack()
+rb_r=Radiobutton(ventana,text="Resta",value="Restar",variable=seleccion)
+rb_r.pack()
+
+boton_calcular =Button(ventana, text="Calcular", command=calcular)
+boton_calcular.pack()
 
 ventana.mainloop()
